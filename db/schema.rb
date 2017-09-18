9cd9b596d3b72dd6b53efc08dc7800e5f0636d63
FukushimaTakeshi <whippet.818@gmail.com>
2017-09-18 09:36:41 +0000

    Add user microposts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170909111147) do
+ActiveRecord::Schema.define(version: 20170918073811) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
